1c39e6247695b34a993a34e38ca86334965c17cc
UltralyticsAssistant <web@ultralytics.com>
2026-04-24 04:30:45 +0000

    Auto-format by https://ultralytics.com/actions

--- train.sh
+++ train.sh
@@ -1,2 +1,1 @@
-nohup yolo train data=face_mask.yaml  model=yolo26n.yaml epochs=300 imgsz=320 batch=64 device=0 > train_421.log 2>&1 &
-
+nohup yolo train data=face_mask.yaml model=yolo26n.yaml epochs=300 imgsz=320 batch=64 device=0 > train_421.log 2>&1 &
